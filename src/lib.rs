80ce52f9ddca8f586bd0e68e6be1e46127fe4fc5
pmarkgraf-bionaut <philip.markgraf@bionautlabs.com>
2022-02-04 11:46:17 -0800

    f Made C to M cases work.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
-pub fn to_roman(_: u32) -> String {
-    "".to_string()
+pub fn to_roman(arabic: u32) -> String {
+    let mut remainder = arabic;
+    let mut result = String::new();
+    while remainder >= 1000 {
+        result.push('M');
+        remainder -= 1000;
+    }
+    if remainder >= 900 {
+        result.push_str("CM");
+        remainder -= 900;
+    }
+    if remainder >= 500 {
+        result.push_str("D");
+        remainder -= 500;
+    }
+    if remainder >= 400 {
+        result.push_str("CD");
+        remainder -= 400;
+    }
+    while remainder >= 100 {
+        result.push('C');
+        remainder -= 100;
+    }
+
+    result
 }
--- tests/roman_numerals_ut.rs
+++ tests/roman_numerals_ut.rs
@@ -4,6 +4,13 @@ mod tests {
 
     #[test]
     fn test_to_roman() {
-        assert_eq!(to_roman(0), "change this test");
+        assert_eq!(to_roman(3000), "MMM");
+        assert_eq!(to_roman(2500), "MMD");
+        assert_eq!(to_roman(1000), "M");
+        assert_eq!(to_roman(900), "CM");
+        assert_eq!(to_roman(500), "D");
+        assert_eq!(to_roman(400), "CD");
+        assert_eq!(to_roman(300), "CCC");
+        assert_eq!(to_roman(100), "C");
     }
 }
